767cd7b0e3b19beebedafd8012a9a530f66c2b70
James Fu <jamesfup@gmail.com>
2024-12-15 20:38:46 -0800

    bug fixes for project page and content + reorder skills

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -20,8 +20,8 @@ export default function page() {
       <div className="h-10 xl:h-32 absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
       <div className="max-w-7xl mx-auto p-5 mt-10">
       <About />
-      <Skills />
       <Project/>
+      <Skills />
       <Contact />
       <Footer/>
       </div>
