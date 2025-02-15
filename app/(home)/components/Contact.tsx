55ab6f7458b3948ba3ac6a7e658e91f909cc9e37
James Fu <jamesfup@gmail.com>
2025-02-14 16:15:01 -0800

    navbar

--- app/(home)/components/Contact.tsx
+++ app/(home)/components/Contact.tsx
@@ -31,7 +31,7 @@ export const Contact = () => {
             </div>
             <div className="flex items-center gap-5">
               <FontAwesomeIcon icon={faMapMarkerAlt} className="w-4 h-4" />
-              <p>SF, CA</p>
+              <p>Bay Area, CA</p>
             </div>
           </div>
         </div>
